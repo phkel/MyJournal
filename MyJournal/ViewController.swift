5c214135cad1221b456a8dba17cac20bceac5fac
Kertu Kipper <phkel@users.noreply.github.com>
2019-03-10 11:10:29 +0200

    Initial Commit

---
+++ MyJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyJournal
+//
+//  Created by Kertu Kipper on 10/03/2019.
+//  Copyright © 2019 Kertu Kipper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
